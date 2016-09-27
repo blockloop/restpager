7b66431f3217714952d7a652492ad2fa040c9ef1
Brett Jones <bjones027+noreply@gmail.com>
2016-09-27 16:11:10 -0500

    add parseHeader

--- src/index.js
+++ src/index.js
@@ -1,10 +1,30 @@
 const URL = require("url");
 const qs = require("querystring");
 
+/**
+ * Weblinks
+ * @typedef {Object} Weblinks
+ * @property {Url} self - url which points to the current page
+ * @property {Url} next - url which points to the next page
+ * @property {Url} prev - url which points to the previous page
+ * @property {Url} first - url which points to the first page
+ * @property {Url} last - url which points to the last page
+ * @property {String} linkHeader - value to be added to the Link header on the response
+ */
+
 module.exports = {
+    /**
+     * create weblinks
+     * @param {String} url - the url which pagination will be appended to
+     * @param {Number} page - the page which will resolve to rel=self
+     * @param {Number} pageSize - how many items per page
+     * @param {Number=} totalCount - how many items total. Defaults to pageSize.
+     * @returns {Weblinks} - resulting weblinks
+     */
     create: (url, page, pageSize, totalCount) => {
+        totalCount = totalCount || pageSize;
+
         const res = {
-            url: url,
             self: makeURL(url, page, pageSize),
             first: makeURL(url, 1, pageSize),
             last: makeURL(url, Math.ceil(totalCount / pageSize), pageSize),
@@ -26,6 +46,30 @@ module.exports = {
                 return `<${res[key]}>; rel="${key}"`;
             })
             .join(",\n");
+
+        return res;
+    },
+    /**
+     * parseHeader parses a Link header
+     * @param {String} headerVal - the value of the Link header
+     * @returns {Weblinks} the resulting links
+     */
+    parseHeader: (headerVal) => {
+        if (!headerVal) {
+            return null;
+        }
+        const lines = headerVal.split(",\n");
+        const res = {};
+
+        const re = /<([^>]+)>; rel="([^"]+)"/;
+        lines.forEach((line) => {
+            const match = re.exec(line);
+            if (match) {
+                const parsed = URL.parse(match[1]);
+                parsed.query = qs.parse(parsed.query);
+                res[match[2]] = parsed;
+            }
+        });
 
         return res;
     }
--- test/build.js
+++ test/build.js
@@ -83,3 +83,31 @@ describe("linkHeader", () => {
         });
     });
 });
+
+describe("parseHeader", () => {
+    it("should parse all lines", () => {
+        const header = [
+            '<http://www.google.com/?page=5&page_size=10>; rel="self"',
+            '<http://www.google.com/?page=6&page_size=10>; rel="next"',
+            '<http://www.google.com/?page=4&page_size=10>; rel="prev"',
+            '<http://www.google.com/?page=1&page_size=10>; rel="first"',
+            '<http://www.google.com/?page=10&page_size=10>; rel="last"',
+        ].join(",\n");
+
+        const links = weblinks.parseHeader(header);
+        expect(links.self).to.have.key("query");
+        expect(links.self.query.page).to.eql(5);
+
+        expect(links.next).to.have.key("query");
+        expect(links.next.query.page).to.eql(6);
+
+        expect(links.prev).to.have.key("query");
+        expect(links.prev.query.page).to.eql(4);
+
+        expect(links.first).to.have.key("query");
+        expect(links.first.query.page).to.eql(1);
+
+        expect(links.last).to.have.key("query");
+        expect(links.last.query.page).to.eql(10);
+    });
+});
